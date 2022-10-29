e0a51608e01f153472384b14255ce2b7a58ebedc
victor <vdcommits@outlook.com>
2022-10-29 22:40:12 +0200

    added time bits

--- ovo-sheets-frontend/src/components/staff-form.tsx
+++ ovo-sheets-frontend/src/components/staff-form.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { ChangeEventHandler, useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, FormLabel, InputGroup } from "react-bootstrap";
 
 
 
@@ -9,14 +10,15 @@ function DropFormGroup(groupNumber: number) {
     let emailName = `Enter Email #${groupNumber + 1}`
     if (groupNumber > 0) {
         return (
-            <Form.Group className="mb-3" controlId={groupName} key={groupName}>
+            <Form.Group className="input-group mb-3" controlId={groupName} key={groupName}>
+                <InputGroup.Text>Email</InputGroup.Text>
                 <Form.Control type="email" placeholder={emailName} />
             </Form.Group>
         )
     } else {
         return (
-            <Form.Group className="mb-3" controlId={groupName} key={groupName}>
-                <Form.Label>Email address</Form.Label>
+            <Form.Group className="input-group mb-3" controlId={groupName} key={groupName}>
+                <InputGroup.Text>Email</InputGroup.Text>
                 <Form.Control type="email" placeholder="Enter email" />
             </Form.Group>
         )
@@ -26,45 +28,45 @@ function DropFormGroup(groupNumber: number) {
 function MonthOptions() {
     // using JS date non human month numbers
     const months = new Map<number, string>();
-    months.set(0,   "January")
-    months.set(1,   "February")
-    months.set(2,   "March")
-    months.set(3,   "April")
-    months.set(4,   "May")
-    months.set(5,   "June")
-    months.set(6,   "July")
-    months.set(7,   "August")
-    months.set(8,   "September")
-    months.set(9,   "October")
-    months.set(10,  "November")
-    months.set(11,  "December")
+    months.set(0, "January")
+    months.set(1, "February")
+    months.set(2, "March")
+    months.set(3, "April")
+    months.set(4, "May")
+    months.set(5, "June")
+    months.set(6, "July")
+    months.set(7, "August")
+    months.set(8, "September")
+    months.set(9, "October")
+    months.set(10, "November")
+    months.set(11, "December")
     return months;
 }
 
-function determineMonths(startingMonth: number){
+function determineMonths(startingMonth: number) {
     const maxMonth = 11;
     const months: Map<number, string> = MonthOptions();
     let options = [];
-    for(let month = startingMonth; month <= maxMonth; month++){        
+    for (let month = startingMonth; month <= maxMonth; month++) {
         let monthValue = months.get(month);
-        options.push(<option key={'month_'+month} value={month}>{monthValue}</option>)
+        options.push(<option key={'month_' + month} value={month}>{monthValue}</option>)
     }
     return options;
-}    
+}
 
 
-function DropDayOption(currentDate: Date, currentDay: number, startingMonth: number){
+function DropDayOption(currentDate: Date, currentDay: number, startingMonth: number) {
     let referenceMonth: Number;
-    if(currentDate.getMonth() !== startingMonth){
+    if (currentDate.getMonth() !== startingMonth) {
         referenceMonth = startingMonth;
-    }else{
+    } else {
         referenceMonth = currentDate.getMonth();
     }
     let nextMonth = Number(referenceMonth) + 1; // casts to number
     let lastDayOfMonth = new Date(currentDate.getFullYear(), nextMonth, 0);
     const days = [];
-    for(let day: number = currentDay; day <= lastDayOfMonth.getDate(); day++){
-        days.push(<option key={'day_'+day} value={day}>{day}</option>)
+    for (let day: number = currentDay; day <= lastDayOfMonth.getDate(); day++) {
+        days.push(<option key={'day_' + day} value={day}>{day}</option>)
     }
     return (
         <Form.Select defaultValue={currentDay} aria-label='Default select example'>
@@ -84,17 +86,59 @@ function DropMonthOption(startingMonth: number, monthFunctionTrigger: ChangeEven
 
 
 
-function DropYearOption(currentDate: Date, yearFunctionTrigger: ChangeEventHandler){
+function DropYearOption(currentDate: Date, yearFunctionTrigger: ChangeEventHandler) {
     const currentYear = currentDate.getUTCFullYear();
-    const nextYear = currentYear+1
+    const nextYear = currentYear + 1
     return (
         <Form.Select defaultValue={currentYear} onChange={yearFunctionTrigger} aria-label='Default select example'>
-            <option key={'year_'+currentYear} value={currentYear}>{currentYear}</option>
-            <option key={'year_'+nextYear} value={nextYear}>{nextYear}</option>
+            <option key={'year_' + currentYear} value={currentYear}>{currentYear}</option>
+            <option key={'year_' + nextYear} value={nextYear}>{nextYear}</option>
         </Form.Select>
     )
 }
 
+
+function DropHourOption() {
+    const hours = [];
+    for (let hour: number = 0; hour < 24; hour++) {
+        hours.push(<option key={'hour_' + hour} value={hour}>{hour}</option>)
+    }
+    return (
+        <Form.Select defaultValue="0" aria-label='Default select example'>
+            {hours}
+        </Form.Select>
+    )
+}
+
+function DropMinuteOption() {
+    const minutes = []
+    for (let minute: number = 0; minute < 60; minute += 15) {
+        minutes.push(<option key={'minutes_' + minute} value={minute}>{minute}</option>)
+    }
+    return (
+        <Form.Select defaultValue="0" aria-label='Default select example'>
+            {minutes}
+        </Form.Select>
+    )
+}
+
+function DropReservationType() {
+    return (
+        <Form.Select aria-label='Default select example'>
+            <option key='type_jaeger_accounts' value='type_jaeger_accounts'>Basic Jaeger Accounts</option>
+            <option key='type_observer_accounts' value='type_observer_accounts'>Observer Accounts</option>
+        </Form.Select>
+    )
+}
+
+function DropAccountStatus() {
+    return (
+        <Form.Select aria-label='Default select example'>
+            <option key='status_not_ready' value='not_ready'>Let Edward inspect the sheet for sharing</option>
+            <option key='status_manually_shared' value='manually_shared'>I am manually sharing sheet</option>
+        </Form.Select>
+    )
+}
 
 
 
@@ -140,51 +184,70 @@ export function OvOStaffForm() {
     }
 
     const onYearChange = (event: any) => {
-        if('target' in event){
+        if ('target' in event) {
             let target = event.target
-            if('value' in target){
-                if(Number(target.value) !== currentDate.getUTCFullYear()){
+            if ('value' in target) {
+                if (Number(target.value) !== currentDate.getUTCFullYear()) {
                     adjustStartingMonth(0)
                     adjustRefMonth(0);
-                }else{
+                } else {
                     adjustStartingDay(currentDay);
                     adjustStartingMonth(currentMonth);
                     adjustRefMonth(currentMonth);
                 }
-            }            
+            }
         }
     }
 
     const onMonthChange = (event: any) => {
-        if('target' in event){
+        if ('target' in event) {
             let target = event.target
-            if('value' in target){
-                if(Number(target.value) !== currentDate.getUTCMonth()){
+            if ('value' in target) {
+                if (Number(target.value) !== currentDate.getUTCMonth()) {
                     adjustStartingDay(1);
                     adjustRefMonth(target.value);
-                }else{
+                } else {
                     adjustStartingDay(currentDay);
                     adjustRefMonth(target.value);
                 }
-            }            
+            }
         }
     }
+
+ 
+    
 
     return (
         <Form>
             {emailFormEntries}
             <Form.Group className="mb-3">
-            <Button variant="info" value="addButton" onClick={onEmailAddClick} disabled={disableAddButton}>+ Email</Button>
-            <Button variant="info" value="removeButton" onClick={onEmailRemoveClick} disabled={disableRemoveButton}>- Email</Button>
+                <Button variant="success" value="addButton" onClick={onEmailAddClick} disabled={disableAddButton}>+ Email</Button>
+                <Button variant="danger" value="removeButton" onClick={onEmailRemoveClick} disabled={disableRemoveButton}>- Email</Button>
             </Form.Group>
 
-            <Form.Group className="input-group mb-3" controlId="formMonth">
-            {DropYearOption(currentDate, onYearChange)}
-            {DropMonthOption(startingMonth, onMonthChange)}
-            {DropDayOption(currentDate, startingDay, refMonth)}
-            
+            <Form.Group className="input-group mb-3" controlId="formDateTime">
+                <InputGroup.Text>Year</InputGroup.Text>
+                {DropYearOption(currentDate, onYearChange)}
+                <InputGroup.Text>Month</InputGroup.Text>
+                {DropMonthOption(startingMonth, onMonthChange)}
+                <InputGroup.Text>Day</InputGroup.Text>
+                {DropDayOption(currentDate, startingDay, refMonth)}
             </Form.Group>
-            
+
+            <Form.Group className="input-group mb-3" controlId="formHour">
+                <InputGroup.Text>Hour</InputGroup.Text>
+                {DropHourOption()}
+                <InputGroup.Text>Minutes</InputGroup.Text>
+                {DropMinuteOption()}
+            </Form.Group>
+            <Form.Group className="input-group mb-3">
+                <InputGroup.Text>Account Type</InputGroup.Text>
+                {DropReservationType()}
+            </Form.Group>
+            <Form.Group className="input-group mb-3">
+                <InputGroup.Text>Share Type</InputGroup.Text>
+                {DropAccountStatus()}
+            </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
